231168d332ffcd3a1c5ca56293117849d3731516
Lindsay Silver <lindsay@lindo.local>
2014-04-01 13:17:20 -0700

    Refactored directives

--- angles.js
+++ angles.js
@@ -1,6 +1,5 @@
 var angles = angular.module("angles", []);
 
-
 angles.chart = function (type) {
     return { 
         restrict: "A",
@@ -9,104 +8,60 @@ angles.chart = function (type) {
             options: "=",
             id: "@",
             width: "=",
-            height: "="
+            height: "=",
+            resize: "=",
+            type: "@"
         },
         link: function ($scope, $elem) {
             var ctx = $elem[0].getContext("2d");
-            var autosize = false
+            var autosize = false;
+            var type = type || $scope.type || "line";
 
-            if ($scope.width <= 0) {
-                $elem.width($elem.parent().width());
-                $elem.height($elem.parent().height());
-                ctx.canvas.width = $elem.width();
-                ctx.canvas.height = ctx.canvas.width / 2;               
-            } else {
-                ctx.canvas.width = $scope.width || ctx.canvas.width;
-                ctx.canvas.height = $scope.height || ctx.canvas.height;
-                autosize = true;
-            }
-
-    
-            var chart = new Chart(ctx);
+			$scope.size = function () {
+	            if ($scope.width <= 0) {
+	                $elem.width($elem.parent().width());
+	                $elem.height($elem.parent().height());
+	                ctx.canvas.width = $elem.width();
+	                ctx.canvas.height = ctx.canvas.width / 2;               
+	            } else {
+	                ctx.canvas.width = $scope.width || ctx.canvas.width;
+	                ctx.canvas.height = $scope.height || ctx.canvas.height;
+	                autosize = true;
+	            }				
+			}
 
             $scope.$watch("data", function (newVal, oldVal) { 
                 // if data not defined, exit
                 if(autosize){
-                    ctx.canvas.width = $scope.width || $elem.parent().width();
-                    ctx.canvas.height = $scope.height || $elem.parent().height();
+                    $scope.size();
                     chart = new Chart(ctx);
                 } else if (!newVal) return;
                 
                 chart[type]($scope.data, $scope.options);
             }, true);
+            
+            if ($scope.resize) {
+		        var w = angular.element($window);
+		
+		        w.bind('resize', function () {
+		            $scope.size();
+		            chart[type]($scope.data, $scope.options);
+		        });	            
+            }
+            
+			$scope.size();
+            var chart = new Chart(ctx);
         }
     }
 }
 
 
-/* General Chart Wrapper */
-angles.directive("chart", function () { 
-    return { 
-        restrict: "A",
-        scope: {
-            data: "=",
-            type: "@",
-            options: "=",
-            id: "@",
-            width: "=",
-            height: "="
-        },
-        link: function ($scope, $elem) {
-            var ctx = $elem[0].getContext("2d");
-            var autosize = false
-
-            if ($scope.width <= 0) {
-                $elem.width($elem.parent().width());
-                $elem.height($elem.parent().height());
-                ctx.canvas.width = $elem.width();
-                ctx.canvas.height = ctx.canvas.width / 2;               
-            } else {
-                ctx.canvas.width = $scope.width || $elem.parent().width();
-                ctx.canvas.height = $scope.height || $elem.parent().height();
-                autosize = true;
-            }
-
-
-            var chart = new Chart(ctx);
-
-            $scope.$watch("data", function (newVal, oldVal) {
-                if(autosize){
-                    ctx.canvas.width = $scope.width || $elem.parent().width();
-                    ctx.canvas.height = $scope.height || $elem.parent().height();
-                    chart = new Chart(ctx);
-                }
-
-                if ($scope.data !== undefined) { 
-                  chart[$scope.type]($scope.data, $scope.options);
-                }
-            }, true);
-        }
-    } 
-});
-
-angles.directive('resize', function ($window) {
-    return function ($scope, $elem) {
-        var w = angular.element($window);
-
-        w.bind('resize', function () {
-            $scope.plotData["refresh"] = !$scope.plotData["refresh"];
-            $scope.$apply();
-        });
-    }
-})
-
-
 /* Aliases for various chart types */
+angles.directive("chart", function () { return angles.chart(); });
 angles.directive("linechart", function () { return angles.chart("Line"); });
 angles.directive("barchart", function () { return angles.chart("Bar"); });
 angles.directive("radarchart", function () { return angles.chart("Radar"); });
 angles.directive("polarchart", function () { return angles.chart("PolarArea"); });
 angles.directive("piechart", function () { return angles.chart("Pie"); });
 angles.directive("doughnutchart", function () { return angles.chart("Doughnut"); });
-/* BC */
 angles.directive("donutchart", function () { return angles.chart("Doughnut"); });
